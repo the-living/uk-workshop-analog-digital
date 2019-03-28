9ebdb17ba326a1097e939d1d390b3e3d9e6d0942
James Stoddart <james.stoddart@autodesk.com>
2019-03-28 17:07:39 -0400

    Calibration script & JSON settings

---
+++ webcam_calibrate.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+
+import os
+import time
+
+from utility import CircleGrid
+from utility import Chessboard
+
+w,h = (1920, 1080)
+
+feed = cv2.VideoCapture(1)
+feed.set(3,h)
+feed.set(4,w)
+
+out_dir = "capture"
+
+
+cb = CircleGrid()
+try:
+    cb.load_calibration("circle_calibration.json")
+except:
+    print("CIRCLE GRID CALIBRATION")
+    cb.calibrate(feed, frames=100)
+    cb.save_calibration("circle_calibration.json")
+
+# print("CHESSBOARD CALIBRATION")
+# cb = Chessboard(x=7,y=7)
+# cb.calibrate(feed, frames=60)
+# cb.save_calibration("chess_calibration.json")
+
+while True:
+    ret, frame = feed.read()
+
+    frame_undistorted = cb.undistort(frame, crop=True)
+
+    cv2.imshow('original', frame)
+    cv2.imshow('undistorted', frame_undistorted)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        print("EXITING...")
+        break
+    if chr(key % 256) == 'x':
+        label = "calibrate_{}.jpg".format(time.time())
+        label_undistort = "calibrate_{}.undistorted.jpg".format(time.time())
+        print("SAVING... {}".format(label))
+        cv2.imwrite(os.path.join(out_dir, label), frame)
+        cv2.imwrite(os.path.join(out_dir, label_undistort), frame_undistorted)
+    if chr(key % 256) == 'c':
+        print("CIRCLE GRID CALIBRATION")
+        cv2.destroyAllWindows()
+        cb = CircleGrid()
+        cb.calibrate(feed, frames=100)
+        cb.save_calibration("circle_calibration.json")
+
+
+feed.release()
+cv2.destroyAllWindows()
